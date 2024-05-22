80d3b2425acf45e01dd278fe8b334134fbb0a26d
Josué Antonio Contreras Guzmán <antonioguzman_dev@hotmail.com>
2024-05-22 00:14:30 -0600

    Añadiendo Componente Formulario de Login

--- src/componentes/FormularioLogin.jsx
+++ src/componentes/FormularioLogin.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
+
 function FormularioLogin() {
+    const [nombre, setNombre] = useState("");
+    const [contraseña, setContraseña] = useState("");
+
     return (
-        <section>
-            
+        <section className="w-80 h-60 grid place-content-center bg-gray-100 rounded-md shadow-lg">
+
+            <h1 className="h-10 grid place-items-center text-lg font-bold	">Bienvenido</h1>
+            <form className="grid place-items-center">
+                <input 
+                className="pl-1.5 h-10 w-56 rounded-md border-gray-300 border-2 mb-2 text-sm"
+                type="text"
+                placeholder="Nombre de usuario"
+                value={nombre}
+                onChange={e => setNombre(e.target.value)}
+                />
+                <input 
+                className="pl-1.5 h-10 w-56 rounded-md border-gray-300 border-2 mb-2 text-sm"
+                type="password"
+                placeholder="Contraseña"
+                value={contraseña}
+                onChange={e => setContraseña(e.target.value)}
+                />
+                <button className="h-9 w-56 bg-gray-50 rounded-md border-gray-100 border-2 shadow-md text-sm">Iniciar Sesión</button>
+            </form>
         </section>
     );
 }
